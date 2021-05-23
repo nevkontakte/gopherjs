nevkontakte/gopherjs-plus

Add cmd/gopherjs-ng binary that wraps around the `go` tool.
In theory, this approach should allow to keep only compiler and linker
parts of GopherJS and rely on the mainline Go to handle all the detail
of the build system, such as modules, file embedding, etc.

This tool work by setting -toolexec flag to point at "gopherjs-ng
adaptor", which then acts as if it was the standard compiler/linker. For
the moment it doesn't actually do anything, but `go build -n` works
successfully.

As of now, this needs a custom version of the go tool, which disables
GOOS/GOARCH pair validity check, since GopherJS uses GOARCH=js. Ideally,
we could get a way to disable that check.

cmd/gopherjs-ng/flags.go
@@ -1,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/goplusjs/gopherjs/compiler"
+)
+
+type commonFlags struct {
+	Version string
+
+	tool string
+}
+
+func (cf *commonFlags) Bind(tool string) *flag.FlagSet {
+	cf.tool = tool
+
+	fs := flag.NewFlagSet(tool, flag.ContinueOnError)
+	fs.StringVar(&cf.Version, "V", cf.Version, "Print tool version and exit.")
+
+	return fs
+}
+
+func (cf *commonFlags) ProcessSpecial() bool {
+	if cf.Version == "full" {
+		fmt.Println(cf.tool, "version", compiler.Version)
+		return true
+	}
+	return false
+}
+
+// repeatedFlag allows passing multiple values to the flag by repeating it in the command line.
+type repeatedFlag []string
+
+func (rf *repeatedFlag) Set(value string) error {
+	*rf = append(*rf, value)
+	return nil
+}
+
+func (rf repeatedFlag) String() string {
+	return fmt.Sprint([]string(rf))
+}

nevkontakte/gopherjs

cmd/gopherjs-ng/flags.go
@@ -1,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/gopherjs/gopherjs/compiler"
+)
+
+type commonFlags struct {
+	Version string
+
+	tool string
+}
+
+func (cf *commonFlags) Bind(tool string) *flag.FlagSet {
+	cf.tool = tool
+
+	fs := flag.NewFlagSet(tool, flag.ContinueOnError)
+	fs.StringVar(&cf.Version, "V", cf.Version, "Print tool version and exit.")
+
+	return fs
+}
+
+func (cf *commonFlags) ProcessSpecial() bool {
+	if cf.Version == "full" {
+		fmt.Println(cf.tool, "version", compiler.Version)
+		return true
+	}
+	return false
+}
+
+// repeatedFlag allows passing multiple values to the flag by repeating it in the command line.
+type repeatedFlag []string
+
+func (rf *repeatedFlag) Set(value string) error {
+	*rf = append(*rf, value)
+	return nil
+}
+
+func (rf repeatedFlag) String() string {
+	return fmt.Sprint([]string(rf))
+}